5af3bca6bd48c05dd93b3e0a7957999253e62c3e
jkloppenburg1 <jkloppenburg1@student.cccd.edu>
2016-11-08 10:41:58 -0800

    misc

--- app/src/main/java/com/example/jkloppenburg1/petprotector/PetListAdapter.java
+++ app/src/main/java/com/example/jkloppenburg1/petprotector/PetListAdapter.java
@@ -3,11 +3,18 @@ package com.example.jkloppenburg1.petprotector;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Nick on 11/7/2016.
  */
 
 public class PetListAdapter extends ArrayAdapter<Pet> {
+
+    private Context mContext;
+    private List<Pet> mPetList = new ArrayList<>();
+    private int mResourceId;
 
     public PetListAdapter(Context c, int)
 }
